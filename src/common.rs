2a4981b893f1c37678e8a5ceb6037c0a4acd93f9
unknown <contact@itok01.com>
2020-03-11 01:50:54 +0900

    Remove rand

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,3 @@
-use rand::prelude::*;
-
 use super::config::*;
 
 // ドメインとパスを合成してURLを生成
@@ -10,19 +8,4 @@ pub fn endpoint(path: &str) -> String {
     } else {
         format!("http://{}{}", config.domain, path)
     }
-}
-
-const BASE_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-// ランダムな文字列を生成
-pub fn gen_random_string(size: usize) -> String {
-    let mut rng = rand::thread_rng();
-    String::from_utf8(
-        BASE_STR
-            .as_bytes()
-            .choose_multiple(&mut rng, size)
-            .cloned()
-            .collect(),
-    )
-    .unwrap()
 }
