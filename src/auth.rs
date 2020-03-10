4d16be013f8b8744b3a3810ff1ca32f9457c29ef
unknown <contact@itok01.com>
2020-03-11 00:29:49 +0900

    Remove tmp user token

--- src/auth.rs
+++ src/auth.rs
@@ -8,17 +8,30 @@ use super::common::*;
 use super::config::*;
 use super::database::*;
 
-// ユーザーの一時的なトークンとリクエストトークン
+// リクエストトークン
 #[derive(Serialize, Deserialize)]
-struct TmpUserToken {
-    tmp_user_token: String,
-    request_token_key: String,
-    request_token_secret: String,
+struct Token {
+    key: String,
+    secret: String,
+}
+
+// ユーザートークン
+#[derive(Serialize, Deserialize)]
+struct UserToken {
+    id: String,
+    token: Token,
+}
+
+// コールバックで受け取る情報
+#[derive(Deserialize)]
+struct CallbackQuery {
+    oauth_token: String,
+    oauth_verifier: String,
 }
 
 // Twitterの認証リンクを生成してリダイレクト
 #[get("/auth")]
-async fn auth(session: Session) -> HttpResponse {
+async fn auth() -> HttpResponse {
     // 設定を読み込む
     let config = load_config();
     let con_token = egg_mode::KeyPair::new(config.consumer_key, config.consumer_secret);
@@ -31,36 +44,24 @@ async fn auth(session: Session) -> HttpResponse {
     // 認証リンクを生成
     let auth_url = egg_mode::authorize_url(&request_token);
 
-    // 一時的なユーザーのコレクションにアクセス
+    // リクエストトークンのコレクションにアクセス
     let database = connect_database();
-    let tmp_user_collection = database.collection("tmp_user_token");
+    let request_token_collection = database.collection("request_token");
 
-    // トークンを生成し、リクエストトークンと結びつける
-    let tmp_user_token = TmpUserToken {
-        tmp_user_token: gen_random_string(32),
-        request_token_key: request_token.key.to_string(),
-        request_token_secret: request_token.secret.to_string(),
+    // リクエストトークンをBsonに変換する
+    let request_token = Token {
+        key: request_token.key.to_string(),
+        secret: request_token.secret.to_string(),
     };
-    let serialized_tmp_user_token = bson::to_bson(&tmp_user_token).unwrap();
-
-    // クッキーにトークンを保存
-    match serialized_tmp_user_token {
-        bson::Bson::Document(document) => {
-            match session.set("tmp_user_token", &tmp_user_token.tmp_user_token) {
+    match bson::to_bson(&request_token).unwrap() {
+        bson::Bson::Document(request_token_doc) => {
+            // データベースにリクエストトークンを保存
+            match request_token_collection.insert_one(request_token_doc, None) {
                 Ok(_) => {
-                    // データベースにトークンを保存
-                    match tmp_user_collection.insert_one(document, None) {
-                        Ok(_) => {
-                            // 認証リンクにリダイレクト
-                            HttpResponse::TemporaryRedirect()
-                                .header(http::header::LOCATION, auth_url)
-                                .finish()
-                        }
-                        Err(e) => {
-                            println!("{:?}", e);
-                            HttpResponse::InternalServerError().finish()
-                        }
-                    }
+                    // 認証リンクにリダイレクト
+                    HttpResponse::TemporaryRedirect()
+                        .header(http::header::LOCATION, auth_url)
+                        .finish()
                 }
                 Err(e) => {
                     println!("{:?}", e);
@@ -72,13 +73,6 @@ async fn auth(session: Session) -> HttpResponse {
     }
 }
 
-// コールバックで受け取る情報
-#[derive(Deserialize)]
-struct CallbackQuery {
-    oauth_token: String,
-    oauth_verifier: String,
-}
-
 // コールバックの処理
 #[get("/callback")]
 async fn callback(web::Query(query): web::Query<CallbackQuery>, session: Session) -> HttpResponse {
@@ -86,50 +80,83 @@ async fn callback(web::Query(query): web::Query<CallbackQuery>, session: Session
     let config = load_config();
     let con_token = egg_mode::KeyPair::new(config.consumer_key, config.consumer_secret);
 
-    let tmp_user_token = match session.get::<String>("tmp_user_token") {
-        Ok(tmp_user_token) => match tmp_user_token {
-            Some(tmp_user_token) => tmp_user_token,
-            None => String::new(),
+    // リクエストトークンのコレクションにアクセス
+    let database = connect_database();
+    let request_token_collection = database.collection("request_token");
+
+    // リクエストトークンを取り出す
+    let filter = doc! {"key": query.oauth_token};
+    match request_token_collection.find_one(filter, None) {
+        Ok(request_token_doc) => match request_token_doc {
+            Some(request_token_doc) => {
+                // BsonをTokenに変換
+                let request_token: Token =
+                    bson::from_bson(bson::Bson::Document(request_token_doc.clone())).unwrap();
+
+                // キーペアの作成
+                let request_token = egg_mode::KeyPair::new(request_token.key, request_token.secret);
+
+                // トークンが有効か確かめる
+                match egg_mode::access_token(con_token, &request_token, query.oauth_verifier).await
+                {
+                    Ok(access_token) => {
+                        // MongoDBがUnsignedに対応していないため、ユーザーIDをStringに変換
+                        let user_id_u64: u64 = access_token.1;
+                        let user_id = format!("{}", user_id_u64);
+                        // ユーザートークンをBsonに変換する
+                        let user_token = UserToken {
+                            id: user_id,
+                            token: Token {
+                                key: request_token.key.to_string(),
+                                secret: request_token.secret.to_string(),
+                            },
+                        };
+                        match bson::to_bson(&user_token).unwrap() {
+                            bson::Bson::Document(request_token_doc) => {
+                                // クッキーにユーザートークンを保存
+                                match session.set("token", &user_token.token.key) {
+                                    Ok(_) => {
+                                        // データベースにリクエストトークンを保存
+                                        let user_token_collection =
+                                            database.collection("user_token");
+                                        match user_token_collection
+                                            .insert_one(request_token_doc, None)
+                                        {
+                                            Ok(_) => {
+                                                // 認証リンクにリダイレクト
+                                                HttpResponse::TemporaryRedirect()
+                                                    .header(http::header::LOCATION, endpoint("/"))
+                                                    .finish()
+                                            }
+                                            Err(e) => {
+                                                println!("{:?}", e);
+                                                HttpResponse::InternalServerError().finish()
+                                            }
+                                        }
+                                    }
+                                    Err(e) => {
+                                        println!("{:?}", e);
+                                        HttpResponse::InternalServerError().finish()
+                                    }
+                                }
+                            }
+                            _ => HttpResponse::InternalServerError().finish(),
+                        }
+                    }
+                    Err(e) => {
+                        println!("{:?}", e);
+                        HttpResponse::InternalServerError().finish()
+                    }
+                }
+            }
+            None => {
+                println!("Tmp user token is not found.");
+                HttpResponse::InternalServerError().finish()
+            }
         },
         Err(e) => {
             println!("{:?}", e);
-            String::new()
+            HttpResponse::InternalServerError().finish()
         }
-    };
-
-    // 一時的なユーザーのコレクションにアクセス
-    let database = connect_database();
-    let tmp_user_token_collection = database.collection("tmp_user_token");
-
-    // 一時的なユーザーのコレクションからリクエストトークンを取り出す
-    let filter = doc! {"tmp_user_token": &tmp_user_token};
-    match tmp_user_token_collection.find_one(filter, None) {
-        Ok(tmp_user_token_doc) => match tmp_user_token_doc {
-            Some(tmp_user_token_doc) => {
-                let doc: TmpUserToken =
-                    bson::from_bson(bson::Bson::Document(tmp_user_token_doc)).unwrap();
-
-                let request_token =
-                    egg_mode::KeyPair::new(doc.request_token_key, doc.request_token_secret);
-
-                let (token, user_id, screen_name) =
-                    egg_mode::access_token(con_token, &request_token, query.oauth_verifier)
-                        .await
-                        .unwrap();
-
-                println!("{:?}", token);
-                println!("{:?}", user_id);
-                println!("{:?}", screen_name);
-
-                egg_mode::tweet::DraftTweet::new("Rustからツイート！こんにちは！")
-                    .send(&token)
-                    .await;
-            }
-            None => println!("Tmp user token is not found."),
-        },
-        Err(e) => println!("{:?}", e),
     }
-    HttpResponse::TemporaryRedirect()
-        .header(http::header::LOCATION, endpoint("/"))
-        .finish()
 }
