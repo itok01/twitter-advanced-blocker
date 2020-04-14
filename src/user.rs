f8920242fe456d74d7d8059a756f14446daf2f3d
itok01 <contact@itok01.com>
2020-04-14 18:15:06 +0900

    Add GET /api/alluser

--- src/user.rs
+++ src/user.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 
 use super::auth::*;
+use super::database::*;
 
 #[derive(Deserialize)]
 pub struct GetUserRequest {
@@ -45,6 +46,35 @@ pub async fn get_user_handler(get_user_request: web::Query<GetUserRequest>) -> H
     }
 }
 
+#[derive(Deserialize)]
+pub struct GetAllUserRequest {
+    token: String,
+}
+
+#[derive(Serialize)]
+pub struct GetAllUserResponse {
+    ok: bool,
+    id: Option<std::vec::Vec<String>>,
+}
+
+// 登録済みユーザーの情報を取得
+pub async fn get_all_user_handler(get_user_request: web::Query<GetAllUserRequest>) -> HttpResponse {
+    // トークンを取得
+    match get_token(get_user_request.token.clone()).await {
+        Ok(_) => HttpResponse::Ok().json(GetAllUserResponse {
+            ok: true,
+            id: Option::from(get_all_user().await),
+        }),
+        Err(e) => {
+            println!("{}", e);
+            HttpResponse::InternalServerError().json(GetAllUserResponse {
+                ok: false,
+                id: None,
+            })
+        }
+    }
+}
+
 // ユーザーの情報を取得
 pub async fn get_user_from_twitter(
     token: &egg_mode::Token,
@@ -54,4 +84,26 @@ pub async fn get_user_from_twitter(
         .await
         .unwrap()
         .response
+}
+
+// 登録済みユーザーのIDを取得
+pub async fn get_all_user() -> std::vec::Vec<String> {
+    let mut user = std::vec::Vec::<String>::new();
+
+    let database = connect_database();
+    let user_token_collection = database.collection("user_token");
+
+    for result in user_token_collection.find(None, None).unwrap() {
+        match result {
+            Ok(user_token_doc) => {
+                let user_token: UserToken =
+                    bson::from_bson(bson::Bson::Document(user_token_doc.clone())).unwrap();
+
+                user.push(user_token.id);
+            }
+            Err(e) => println!("{}", e),
+        }
+    }
+
+    user
 }
